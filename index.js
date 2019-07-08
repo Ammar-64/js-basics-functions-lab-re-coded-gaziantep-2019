1bd62e6e279e6ab4c406ffa0eba173f76bcc5522
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-08 13:21:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ function distanceTravelledInFeet(sblock, eblock){
 function calculatesFarePrice(sblock, eblock){
   let distance = Math.abs(sblock - eblock) * 264;
   if (distance <= 400){
-    return 'your ride is on the house';
+    return 0;
   }
   else if (distance > 400 && distance <= 2000) {
-    return 'your fees is '(distance - 400)*0.02;
+    return (distance - 400)*0.02;
   }
   else if (distance > 2000 && distance <= 2500){
-    return 'your fees is 25';
+    return 25;
   }
   else {
     return 'cannot travel that far'
