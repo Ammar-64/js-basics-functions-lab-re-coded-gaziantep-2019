1a1928459bbda0e0201df7efc9961d9b9862b9f7
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-08 13:11:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function distanceFromHqInBlocks(dblock){
 function distanceFromHqInFeet(dblock){
   return Math.abs(dblock - 42) * 264;
 }
-function distanceTravelledInFeet(dblock){
-  return Math.abs(dblock - 42) * 264;
+function distanceTravelledInFeet(sblock, eblock){
+  return Math.abs(sblock - eblock) * 264;
 }
